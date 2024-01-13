644e55e07b30deb50b347f03515ef70408d6fbee
frankcrest <fangg.chen@mail.utoronto.ca>
2024-01-12 20:29:34 -0500

    Convert Video models into AVPlayerItems and load first AVPlayerItem into AVPlayer

--- VideoPlayerSwiftUI/ViewModels/VideoPlayerViewModel.swift
+++ VideoPlayerSwiftUI/ViewModels/VideoPlayerViewModel.swift
@@ -25,11 +25,27 @@ class VideoPlayerViewModel: ObservableObject {
         networkManager.fetchVideos { result in
             switch result {
             case .success(let videos):
-                self.videos = videos.sorted(by: {$0.publishedAt < $1.publishedAt })
+                self.videos = videos.sorted(by: { $0.publishedAt < $1.publishedAt })
+                self.setupPlayer()
             case .failure(let error):
                 self.errorMessage = error.localizedDescription
             }
         }
+    }
+    
+    func setupPlayer() {
+        let playerItems: [AVPlayerItem] = videos.compactMap { video in
+            if let hlsURL = URL(string: video.hlsURL) {
+                return AVPlayerItem(url: hlsURL)
+            } else if let fullURL = URL(string: video.fullURL) {
+                return AVPlayerItem(url: fullURL)
+            } else {
+                return nil
+            }
+        }
+        
+        self.playerItems = playerItems
+        player = AVPlayer(playerItem: playerItems.first)
     }
     
 }
